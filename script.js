06d7225b58264dad873f1a5d16812d8b092271d6
m019m1 <50258878+m019m1@users.noreply.github.com>
2019-08-28 23:40:57 +0700

    rewrite to OOP-style, make it as plugin. Still awful

--- script.js
+++ script.js
@@ -1,4 +1,68 @@
-let loupe;
+class magnifyingGlass {
+	constructor(elem) {
+		this._img = elem;
+		this.getListeners();
+	}
+	getListeners() {
+		this.getOriginalWidth();
+		this._img.addEventListener("mouseenter", showLoupe);
+		let loupe, top, left, right, bottom, scale;
+		let img = this._img;
+		let originalWidth = this._originalWidth;
+
+		function showLoupe(event) {
+			loupe = document.createElement("div");
+			loupe.className = 'loupe';
+
+/* 			let [newX, newY] = getPosition(img, event);
+			loupe.style.background = `url('Treehouse.jpg') no-repeat ${-newX}px ${-newY}px`;
+			moveAt(loupe, event.pageX, event.pageY); */
+			onMouseMove(event);
+			document.body.append(loupe);
+			img.removeEventListener("mouseenter", showLoupe);
+			document.addEventListener("mousemove", onMouseMove);
+		}
+		function getPosition(elem, event) {
+			left = elem.getBoundingClientRect().left;
+			right = elem.getBoundingClientRect().right;
+			top = elem.getBoundingClientRect().top;
+			bottom = elem.getBoundingClientRect().bottom;
+			scale = originalWidth / (right - left);
+			return [(event.clientX - left) * scale - 100, (event.clientY - top) * scale - 100];
+		}
+		function moveAt(elem, pageX, pageY){
+			elem.style.left = `${pageX}px`;
+			elem.style.top = `${pageY}px`;
+		}
+		function onMouseMove(e) {
+			moveAt(loupe, e.pageX, e.pageY); 
+			let [newX, newY] = getPosition(img, event);
+			loupe.style.background = `url('${img.getAttribute("src")}') no-repeat ${-newX}px ${-newY}px`;
+			if(e.clientX < left || e.clientX > right || e.clientY < top || e.clientY > bottom) {
+				document.removeEventListener("mousemove", onMouseMove);
+				loupe.remove();
+				img.addEventListener("mouseenter", showLoupe);
+			}
+		}
+	}
+	getOriginalWidth() {
+		let helperImg = document.createElement("img");
+		helperImg.className = "helperImg";
+		helperImg.setAttribute("src", this._img.getAttribute("src"));
+		document.body.after(helperImg);
+		this._originalWidth = helperImg.getBoundingClientRect().right - helperImg.getBoundingClientRect().left;
+		helperImg.remove();
+	}
+}
+// let image = new magnifyingGlass(document.querySelectorAll("img")[0]);
+let images = document.querySelectorAll("img");
+let loupes = [];
+for(image of images) {
+	loupes.push(new magnifyingGlass(image));
+}
+
+
+/* let loupe;
 let img1 = document.querySelector("img");
 let originalWidth;
 img1.addEventListener("mouseenter", showLoupe);
@@ -7,12 +71,12 @@ let top1, left, right, bottom, scale;
 function showLoupe() {
 	loupe = document.createElement("div");
 	loupe.className = 'loupe';
-	let img2 = document.createElement("img");
-	img2.className = "helperImg";
-	img2.setAttribute("src", img1.getAttribute("src"));
-	document.body.after(img2);
-	originalWidth = img2.getBoundingClientRect().right - img2.getBoundingClientRect().left;
-	img2.remove();
+	let helperImg = document.createElement("img");
+	helperImg.className = "helperImg";
+	helperImg.setAttribute("src", img1.getAttribute("src"));
+	document.body.after(helperImg);
+	originalWidth = helperImg.getBoundingClientRect().right - helperImg.getBoundingClientRect().left;
+	helperImg.remove();
 
 	let [newX, newY] = getPosition(img1, event);
 	loupe.style.background = `url('Treehouse.jpg') no-repeat ${-newX}px ${-newY}px`;
@@ -48,3 +112,4 @@ function onMouseMove(e) {
 	}
 
 }
+ */
