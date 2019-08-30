d891d5ea602611fd2aad6c005ff60bd2e3ef14ca
m019m1 <50258878+m019m1@users.noreply.github.com>
2019-08-30 20:24:58 +0700

    optimizing. fixed: wrong coords

--- script.js
+++ script.js
@@ -5,39 +5,57 @@ class magnifyingGlass {
 		this.run();
 	}
 	run() {
-		let {left, right, top, bottom} = this._img.getBoundingClientRect();
-		let scale;
+		let {left, right, top, bottom} = getCoords(this._img);
+		let originalWidth, scale;
 		let self = this;
-
+	
 		this._img.addEventListener("mouseenter", showLoupe);
 
-		function showLoupe(event) {
-			self.getOriginalWidth();
+		function getCoords(elem) {
+			let box = elem.getBoundingClientRect();
+		
+			return {
+				//expand border to 1px for correct border conditions
+				top: Math.round(box.top) - 1 + pageYOffset,
+				bottom: Math.round(box.bottom) + 1 + pageYOffset,
+				left: Math.round(box.left) - 1 + pageXOffset,
+				right: Math.round(box.right) + 1 + pageXOffset
+			};
+		}
+
+		function showLoupe(e) {
 			self._loupe = document.createElement("div");
 			self._loupe.className = 'loupe';
 			self._loupe.style.width = self._loupe.style.height = `${self._loupeRadius * 2}px`;
 			if(!self._loupe.style.background) {
 				self._loupe.style.background =  `url('${self._img.getAttribute("src")}') no-repeat `;
 			}
-			onMouseMove(event);
+			onMouseMove(e);
 			document.body.append(self._loupe);
 			self._img.removeEventListener("mouseenter", showLoupe);
 			document.addEventListener("mousemove", onMouseMove);
+			
 		}
 		function onMouseMove(e) {
-			moveAt(self._loupe, e.pageX, e.pageY); 
-			changePosition(e);
+			moveAt(self._loupe, e.pageX, e.pageY);
+			changeBackgroundPosition(e);
 			
-			if(e.clientX < left || e.clientX > right || e.clientY < top || e.clientY > bottom) {
+			// border conditions
+			if(e.pageX < left || e.pageX > right || e.pageY < top || e.pageY > bottom) {
 				self._loupe.remove();
 				self._img.addEventListener("mouseenter", showLoupe);
 				document.removeEventListener("mousemove", onMouseMove);
 			}
 		}
-		function changePosition(event) {
-			scale = self._originalWidth / (right - left);
-			let [newX, newY] =  [(event.clientX - left) * scale - self._loupeRadius, 
-												   (event.clientY - top) * scale - self._loupeRadius];
+		function changeBackgroundPosition(event) {
+			if(!originalWidth) {
+				let helperImg = document.createElement("img");
+				helperImg.src = self._img.src;
+				originalWidth = helperImg.width;
+			}
+			scale = originalWidth / (right - left);
+			let [newX, newY] =  [(event.pageX - left) * scale - self._loupeRadius, 
+												   (event.pageY - top) * scale - self._loupeRadius];
 			self._loupe.style.backgroundPosition = `${-newX}px ${-newY}px`;
 		}
 		function moveAt(elem, pageX, pageY){
@@ -45,19 +63,8 @@ class magnifyingGlass {
 			elem.style.top = `${pageY}px`;
 		}
 	}
-
-	// this function may be useful outside
-	getOriginalWidth() {
-		 // for speed-up get originalWidth only when entered the image and only once
-		if(this._originalWidth) {
-			return;
-		}
-		let helperImg = document.createElement("img");
-		helperImg.className = "helperImg";
-		helperImg.src = this._img.src;
-		this._originalWidth = helperImg.width;
-	}
 }
+
 let images = document.querySelectorAll("img");
 for(let image of images) {
 	image = new magnifyingGlass(image, 120);
