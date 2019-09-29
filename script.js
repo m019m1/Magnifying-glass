cce2e4094ccd953f3f96402e57b3a9636d247bd2
m019m1 <50258878+m019m1@users.noreply.github.com>
2019-09-29 22:00:17 +0700

    made it mobile-friendly

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ class magnifyingGlass {
 		let self = this;
 	
 		this._img.addEventListener("mouseenter", showLoupe);
+		this._img.addEventListener("touchstart", showLoupe);
+		this._img.addEventListener("touchend", removeLoupe);
 
 		function getCoords(elem) {
 			let box = elem.getBoundingClientRect();
@@ -30,32 +32,41 @@ class magnifyingGlass {
 			if(!self._loupe.style.background) {
 				self._loupe.style.background =  `url('${this.getAttribute("src")}') no-repeat `;
 			}
-			onMouseMove(e);
+			onPointerMove(e);
 			document.body.append(self._loupe);
 			this.removeEventListener("mouseenter", showLoupe);
-			document.addEventListener("mousemove", onMouseMove);
+			this.removeEventListener("touchstart", showLoupe);
+			document.addEventListener("mousemove", onPointerMove);
+			document.addEventListener("touchmove", onPointerMove);
 			
 		}
-		function onMouseMove(e) {
-			moveAt(self._loupe, e.pageX, e.pageY);
-			changeBackgroundPosition(e);
-			
-			// border conditions
-			if(e.pageX < left || e.pageX > right || e.pageY < top || e.pageY > bottom) {
-				self._loupe.remove();
-				self._img.addEventListener("mouseenter", showLoupe);
-				document.removeEventListener("mousemove", onMouseMove);
+		function onPointerMove() {
+			let pageX = ( (event.type == 'touchmove' || event.type == 'touchstart') ? event.touches[0].pageX : event.pageX);
+			let pageY = ((event.type == 'touchmove' || event.type == 'touchstart') ? event.touches[0].pageY : event.pageY);
+			moveAt(self._loupe, pageX, pageY);
+			changeBackgroundPosition(pageX, pageY);
+			event.preventDefault();
+			// 'mouseleave' event doesn't work here, so it's need to check border conditions
+			if(pageX < left || pageX > right || pageY < top || pageY > bottom) {
+				removeLoupe();
 			}
 		}
-		function changeBackgroundPosition(event) {
+		function removeLoupe() {
+			self._loupe.remove();
+			self._img.addEventListener("mouseenter", showLoupe);
+			self._img.addEventListener("touchstart", showLoupe);
+			document.removeEventListener("mousemove", onPointerMove);
+			document.removeEventListener("touchmove", onPointerMove);
+		}
+		function changeBackgroundPosition(pageX, pageY) {
 			if(!originalWidth) {
 				let helperImg = document.createElement("img");
 				helperImg.src = self._img.src;
 				originalWidth = helperImg.width;
 			}
 			scale = originalWidth / (right - left);
-			let [newX, newY] =  [(event.pageX - left) * scale - self._loupeRadius, 
-												   (event.pageY - top) * scale - self._loupeRadius];
+			let [newX, newY] =  [(pageX - left) * scale - self._loupeRadius, 
+												   (pageY - top) * scale - self._loupeRadius];
 			self._loupe.style.backgroundPosition = `${-newX}px ${-newY}px`;
 		}
 		function moveAt(elem, pageX, pageY){
@@ -65,7 +76,9 @@ class magnifyingGlass {
 	}
 }
 
-let images = document.querySelectorAll("img");
-for(let image of images) {
-	image = new magnifyingGlass(image, 120);
+window.onload = () => {
+  let images = document.querySelectorAll("img");
+  for(let image of images) {
+  	image = new magnifyingGlass(image, 120);
+  }
 }
